c5b36938ff4113e9171e3ed5ea43a036763f962b
hoshianaaa <ryotaro.hoshina@gmail.com>
2022-12-23 12:22:24 +0900

    update

--- src/blob.cpp
+++ src/blob.cpp
@@ -7,6 +7,12 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/opencv.hpp"
+
+// (ref) http://library.isr.ist.utl.pt/docs/roswiki/cv_bridge(2f)Tutorials(2f)UsingCvBridgeToConvertBetweenROSImagesAndOpenCVImages.html
+namespace enc = sensor_msgs::image_encodings;
+using namespace cv;
+using namespace std;
  
 class ImageConverter
 {
@@ -27,28 +33,88 @@ public:
   }
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
-    cv_bridge::CvImagePtr cv_ptr;
+
+    cv_bridge::CvImageConstPtr cv_ptr;
+ //   cv::Mat gray;
     try
     {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      if (enc::isColor(msg->encoding))
+        cv_ptr = cv_bridge::toCvShare(msg, enc::MONO8);
+        //cv::cvtColor(cv_ptr->image, gray, cv::COLOR_BGR2GRAY);
+      else
+        cv_ptr = cv_bridge::toCvShare(msg, enc::MONO8);
+//        gray = cv_ptr->image;
     }
     catch (cv_bridge::Exception& e)
     {
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      // circle (ref.) http://opencv.jp/opencv-2svn/cpp/drawing_functions.html
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0), 3);
-    // Output modified video stream
+
+    cv::Mat im = cv_ptr->image;
+
+	// Setup SimpleBlobDetector parameters.
+	SimpleBlobDetector::Params params;
+
+	// Change thresholds
+	params.minThreshold = 10;
+	params.maxThreshold = 200;
+
+	// Filter by Area.
+	params.filterByArea = true;
+	params.minArea = 100;
+
+	// Filter by Circularity
+	params.filterByCircularity = true;
+	params.minCircularity = 0.1;
+
+	// Filter by Convexity
+	params.filterByConvexity = true;
+	params.minConvexity = 0.87;
+
+	// Filter by Inertia
+	params.filterByInertia = true;
+	params.minInertiaRatio = 0.01;
+
+
+	// Storage for blobs
+	vector<KeyPoint> keypoints;
+
+
+#if CV_MAJOR_VERSION < 3   // If you are using OpenCV 2
+
+	// Set up detector with params
+	SimpleBlobDetector detector(params);
+
+	// Detect blobs
+	detector.detect( im, keypoints);
+#else 
+
+	// Set up detector with params
+	Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);   
+
+	// Detect blobs
+	detector->detect( im, keypoints);
+#endif 
+
+	// Draw detected blobs as red circles.
+	// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures
+	// the size of the circle corresponds to the size of blob
+
+	Mat im_with_keypoints;
+	drawKeypoints( im, keypoints, im_with_keypoints, Scalar(0,0,255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+
+	// Show blobs
+	imshow("keypoints", im_with_keypoints );
+  waitKey(10);
+
     image_pub_.publish(cv_ptr->toImageMsg());
   }
 };
  
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "image_converter");
+  ros::init(argc, argv, "blob");
   ImageConverter ic;
   ros::spin();
   return 0;
